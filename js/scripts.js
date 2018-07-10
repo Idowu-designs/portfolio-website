1ccfd16f2b6e60e303bd7aaf42a9589d30c1b9ad
ID\Idade <idowuadesina79@gmail.com>
2018-07-10 21:45:38 +0200

    Adjusted JS file to remove Alert box and errors on page

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,9 @@
-// Function for Alert box
-alert("Welcome to my World of Design!");
-console.log("Welcome to my World of Design!");
-
 // Hover Function for Project
-$(document).ready(function() {
-  $(".project").on("mouseenter", function() {
-    $(this).find(".project-details").fadeIn();
-    });
-  $(".project").on("mouseleave", function() {
-    $(this).find(".project-details").fadeOut();
-    });
-});
+$('document').ready(function () {
+  $('.project').on('mouseenter', function () {
+    $('this').find('.project-details').fadeIn()
+  })
+  $('.project').on('mouseleave', function () {
+  $ ('this').fadeOut('.project-details').find()
+  })
+})
