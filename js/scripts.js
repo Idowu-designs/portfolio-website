cd2995e53bc1068a181f1bb96bce0a755e64dbdb
ID\Idade <idowuadesina79@gmail.com>
2018-07-20 16:04:55 +0200

    Remove ' ' in JS Scripts

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,9 @@
 // Hover Function for Project
-$('document').ready(function () {
+$(document).ready(function () {
   $('.project').on('mouseenter', function () {
-    $('this').find('.project-details').fadeIn()
+    $(this).find('.project-details').fadeIn()
   })
   $('.project').on('mouseleave', function () {
-    $('this').fadeOut('.project-details').find()
+    $(this).fadeOut('.project-details').find()
   })
 })
